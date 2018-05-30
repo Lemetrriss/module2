b6d4d3be7b7f470b8ca2c5903e0ae3305e4b836a
lemetrriss <lemetrriss@gmail.com>
2018-05-31 01:00:53 +0300

    почему-то пропускает первую итерацию и спрашивает ввод имени только со второго работника?

--- src/classWork/Accountant.java
+++ src/classWork/Accountant.java
@@ -1,8 +1,36 @@
 package classWork;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Accountant {
+
+    private static Scanner sc = new Scanner(System.in);
+    private static Employee[] employees = new Employee[sc.nextInt()];
+
     public static void main(String[] args) {
-        Employee e = new Employee();
-        e.calcSalary("Vasya", 21, 644, 545, 764, 675, 74.765);
+        Random r = new Random();
+        Object[][] table = new Object[employees.length][7];
+
+        for (int i = 0; i < table.length; i++) {
+
+            table[i][0] = sc.nextLine();  // почему-то пропускает первую итерацию и спрашивает ввод имени только со второго работника?
+
+            for (int j = 1; j < table[i].length; j++) {
+                table[i][j] = 10000*r.nextDouble();
+            }
+        }
+
+        for (int i = 0; i < table.length; i++) {
+
+            employees[i] = new Employee((String)table[i][0]);
+
+            double[] salary = new double[table[i].length-1];
+
+            for (int j = 1; j < table[i].length; j++) {
+                salary[j-1] = (double)table[i][j];
+            }
+            employees[i].calcSalary(salary);
+        }
     }
 }
--- src/classWork/Employee.java
+++ src/classWork/Employee.java
@@ -1,12 +1,18 @@
 package classWork;
 
-public class Employee {
-    public void calcSalary(String name, double... salary){
+class Employee {
+    private String name;
+
+    Employee(String name) {
+        this.name = name;
+    }
+
+    void calcSalary(double... salary){
         System.out.print(name+" - ");
         double summ = 0;
         for (double v : salary) {
             summ+=v;
         }
-        System.out.println(summ+" hrn");
+        System.out.println(String.format("%.2f",summ)+" hrn");
     }
 }
