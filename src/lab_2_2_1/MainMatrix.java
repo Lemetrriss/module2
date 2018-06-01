d3519766cb0a890c071b520753c2632ef827c7e2
lemetrriss <lemetrriss@gmail.com>
2018-06-01 15:21:33 +0300

    Задача вычисление площади

--- src/lab_2_2_1/MainMatrix.java
+++ src/lab_2_2_1/MainMatrix.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class MainMatrix {
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         sc.close();
