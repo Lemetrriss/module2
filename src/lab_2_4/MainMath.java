b3e0c9763cb71162d6b3e204c738ad402b58b1ec
lemetrriss <lemetrriss@gmail.com>
2018-06-01 21:09:09 +0300

    2_4_4

--- src/lab_2_4/MainMath.java
+++ src/lab_2_4/MainMath.java
@@ -1,12 +1,18 @@
 package lab_2_4;
 
 import lab_2_4.task.Calculation;
+import lab_2_4.task.Employee;
 import lab_2_4.task.MyMath;
 
 public class MainMath {
     public static void main(String[] args) {
         Calculation.calc();
         System.out.println("MyMath.areaOfCircle(10) = " + MyMath.areaOfCircle(10));
+
+        Employee e1 = new Employee("Ivan", "Ivanov", "engineer", "123");
+        Employee e2 = new Employee("Ivan", "Ivanov", "engineer", "123");
+        Employee e3 = new Employee("Ivan", "Ivanov", "engineer", "123");
+        Employee e4 = new Employee();
+        System.out.println(Employee.getNumberOfEmployeess());
     }
 }
-
---
+++ src/lab_2_4/task/Employee.java
@@ -0,0 +1,58 @@
+package lab_2_4.task;
+
+public class Employee {
+    private String firstName;
+    private String lasttName;
+    private String occupation;
+    private String phone;
+
+    private static int numberOfEmployeess;
+
+    public Employee(String firstName, String lasttName, String occupation, String phone) {
+        this.firstName = firstName;
+        this.lasttName = lasttName;
+        this.occupation = occupation;
+        this.phone = phone;
+        numberOfEmployeess++;
+    }
+
+    public Employee() {
+        this("noname", "-","-","-");
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLasttName() {
+        return lasttName;
+    }
+
+    public void setLasttName(String lasttName) {
+        this.lasttName = lasttName;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public static int getNumberOfEmployeess() {
+        return numberOfEmployeess;
+    }
+}
