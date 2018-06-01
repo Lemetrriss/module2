0c37c052b621f231fc4500828eec70103b3e4f43
lemetrriss <lemetrriss@gmail.com>
2018-06-01 18:58:29 +0300

    --

--- src/lab_2_3_1/MyWindow.java
+++ src/lab_2_3_1/MyWindow.java
@@ -1,5 +1,7 @@
 package lab_2_3_1;
 
+import java.util.Arrays;
+
 public class MyWindow {
     private double width;
     private double height;
@@ -15,11 +17,17 @@ public class MyWindow {
         this.isOpen = isOpen;
     }
 
-    public static void main(String[] args) {
-        MyWindow mw = new MyWindow(12.31, 45.67, 2, "white", true);
-        System.out.println(mw);
-        MyWindow mw1 = new MyWindow();
-        System.out.println(mw1);
+    public MyWindow(double width, double height) {
+            this(width, height, 3, "Black", true);
+            this.width = width;
+            this.height = height;
+    }
+
+    public MyWindow(int numberOfGlass, String color, boolean isOpen) {
+        this(345.53, 98.8746, numberOfGlass, color, isOpen);
+        this.numberOfGlass = numberOfGlass;
+        this.color = color;
+        this.isOpen = isOpen;
     }
 
     public MyWindow() {
@@ -28,6 +36,22 @@ public class MyWindow {
         numberOfGlass = 3;
         color = "Red";
         isOpen = false;
+    }
+
+    public static void main(String[] args) {
+        MyWindow mw = new MyWindow(12.31, 45.67, 2, "white", true);
+        System.out.println(mw);
+        MyWindow mw1 = new MyWindow();
+        System.out.println(mw1);
+        System.out.println();
+        MyWindow[] arr = new MyWindow[3];
+        arr[0] = new MyWindow();
+        arr[1] = new MyWindow(87.866, 98.7644);
+        arr[2] = new MyWindow(7, "Blue", false);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
     }
 
     @Override
