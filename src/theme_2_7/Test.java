4074655971aaacebf580643dc5342c042c2ed2d4
Lemetrriss <lemetrriss@gmail.com>
2018-06-10 22:27:29 +0300

    2_7_5

---
+++ src/theme_2_7/Test.java
@@ -0,0 +1,21 @@
+package theme_2_7;
+
+abstract class Test {
+    int maxSpeed = 160;
+    abstract void test();
+}
+
+class Test2 extends Test{
+    int maxSpeed = 280;
+    void test(){
+        System.out.println(maxSpeed);
+        System.out.println(super.maxSpeed);
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        Test t = new Test2();
+        t.test();
+    }
+}
